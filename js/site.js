201ae874aa940b71ef4c29a0df9b29f05fba4869
vicmo616 <80283011+vicmo616@users.noreply.github.com>
2021-09-30 20:59:32 -0400

    Initial Commit
    initial commit

--- js/site.js
+++ js/site.js
@@ -1,4 +1,34 @@
-//call hello world
-function HelloWorld() {
-    alert("hello world");
+//get string from the page
+//controller function
+function getValue(){
+    
+    document.getElementById("alert").classList.add("invisible");
+   
+    let userString = document.getElementById("userString").value;
+    
+    let revString = reverseString(userString);
+    
+    displayString(revString);
+}
+
+//reverse the string
+//logic function
+function reverseString(userString){
+    let revString = [];
+
+    //reverse a string using a for loop
+    for (let index = userString.length -1 ; index >= 0; index--) {
+        revString += userString[index];
+        
+    }
+    return revString;
+}
+
+//display reversed string to the page
+//view function
+function displayString(revString){
+    //write message to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
